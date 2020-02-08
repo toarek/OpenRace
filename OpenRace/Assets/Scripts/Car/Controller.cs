68db62989c671dbfa6c8ba594a27d196b7eee0d2
toarek <toarek@tlen.pl>
2020-02-09 00:17:43 +0100

    ADD Test Torque for new car controller

---
+++ OpenRace/Assets/Scripts/Car/Controller.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour
+{
+    public Transform wheelR;
+    public Transform wheelL;
+
+    public Rigidbody body;
+
+    public Text text;
+    public Text text1;
+
+    bool to100 = true;
+    float to100Time = 0;
+
+    private void FixedUpdate() {
+
+        wheelR.Rotate(new Vector3(0, -Input.GetAxis("Vertical"), 0));
+        wheelL.Rotate(new Vector3(0, -Input.GetAxis("Vertical"), 0));
+
+        if(isGrounded(wheelR))
+            body.AddForceAtPosition(transform.forward * 400000f * Input.GetAxis("Vertical") * Time.deltaTime, wheelR.position);
+        if(isGrounded(wheelL))
+            body.AddForceAtPosition(transform.forward * 400000f * Input.GetAxis("Vertical") * Time.deltaTime, wheelL.position);
+
+        float speed = body.velocity.magnitude * 3.6f;
+        text.text = "Real Speed: " + Mathf.RoundToInt(speed).ToString();
+
+        if(to100) {
+            if(speed > 1)
+                to100Time += Time.deltaTime;
+            text1.text = "Time to 100 km/h: " + (Mathf.RoundToInt(to100Time * 100f) / 100f).ToString();
+            if(speed >= 100)
+                to100 = false;
+        }
+
+    }
+
+    bool isGrounded(Transform transform) {
+        return Physics.Raycast(transform.position, -Vector3.up, 0.51f);
+    }
+}
--- OpenRace/Assets/Scripts/Car/Test.cs
+++ OpenRace/Assets/Scripts/Car/Test.cs
@@ -19,6 +19,21 @@ namespace OpenRace.Car {
         public float maxSteeringAngle;
 
         public Text text;
+        public Text text1;
+        public Text text2;
+        public Text text3;
+        public Text text4;
+        public Text text5;
+
+        int maxRPM = 1900;
+        float maxSpeed = 350f;
+
+        public WheelCollider WheelL;
+        public WheelCollider WheelR;
+        public float AntiRoll = 0.0f;
+
+        bool to100 = true;
+        float to100Time = 0;
 
         void Awake() {
 
@@ -38,6 +53,16 @@ namespace OpenRace.Car {
             for(int i = 0; i < torqueWheels.Count; i++) {
                 torqueWheels[i].motorTorque = motor;
                 torqueWheelsTransform[i].Rotate(new Vector3(0, torqueWheels[i].rpm / 60 * 360 * Time.deltaTime, 0));
+
+                if(torqueWheels[i].rpm > maxRPM) {
+                    torqueWheels[i].motorTorque = 0;
+                }
+
+                if((100f * torqueWheels[i].rpm * 0.001885f) > maxSpeed) {
+                    torqueWheels[i].brakeTorque = 100000;
+                } else {
+                    torqueWheels[i].brakeTorque = 0;
+                }
             }
 
             for(int i = 0; i < steeringWheels.Count; i++) {
@@ -56,11 +81,47 @@ namespace OpenRace.Car {
                 }
             }
 
-            float circumFerence = 2.0f * 3.14f * 0.25f;
-            float speedOnKmh = (circumFerence * torqueWheels[0].rpm) * 60;
+            float speedOnKmh = 100f * steeringWheels[0].rpm * 0.001885f;
+            float speedOnKmhTorque = 100f * torqueWheels[0].rpm * 0.001885f;
+            float speed = this.GetComponent<Rigidbody>().velocity.magnitude * 3.6f;
 
-            text.text = speedOnKmh.ToString();
+            text.text = "RPM: " + Mathf.RoundToInt(steeringWheels[0].rpm).ToString();
+            text3.text = "Torque RPM: " + Mathf.RoundToInt(torqueWheels[0].rpm).ToString();
+            text1.text = "Wheel Speed: " + Mathf.RoundToInt(speedOnKmh).ToString();
+            text4.text = "Torque Wheel Speed: " + Mathf.RoundToInt(speedOnKmhTorque).ToString();
+            text2.text = "Real Speed: " + Mathf.RoundToInt(speed).ToString();
 
+            if(false) {
+                WheelHit hit;
+                float travelL = 1.0f;
+                float travelR = 1.0f;
+
+                bool groundedL = WheelL.GetGroundHit(out hit);
+                if(groundedL)
+                    travelL = (-WheelL.transform.InverseTransformPoint(hit.point).y - WheelL.radius) / WheelL.suspensionDistance;
+
+                bool groundedR = WheelR.GetGroundHit(out hit);
+                if(groundedR)
+                    travelR = (-WheelR.transform.InverseTransformPoint(hit.point).y - WheelR.radius) / WheelR.suspensionDistance;
+
+                float antiRollForce = (travelL - travelR) * AntiRoll;
+
+                if(groundedL)
+                    this.GetComponent<Rigidbody>().AddForceAtPosition(WheelL.transform.up * -antiRollForce,
+                           WheelL.transform.position);
+
+                if(groundedR)
+                    this.GetComponent<Rigidbody>().AddForceAtPosition(WheelR.transform.up * antiRollForce,
+                           WheelR.transform.position);
+            }
+
+            if(to100) {
+                if(speed > 1)
+                    to100Time += Time.deltaTime;
+                text5.text = "Time to 100 km/h: " + (Mathf.RoundToInt(to100Time*100f)/100f).ToString();
+                if(speed >= 100)
+                    to100 = false;
+            }
         }
     }
 }
